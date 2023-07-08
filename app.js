48500f3f46f4af3867629bca3c656a30d3c278fe
Jahid Hasan <hackerphone630@gmail.com>
2023-07-08 19:26:35 +0600

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+    // $("header_click").click(function(){
+    //   $("slide_content").slideToggle();
+    // });
+  function toggleFaq(content){
+    $(`#${content}`).slideToggle();
+  }
+
+//   });
+
